ba7275330852b516dab9107d12e375c4efd39319
Claudio Anjos <claudio.anjoss@outlook.com>
2023-10-03 16:59:52 -0300

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import styles from './Navbar.module.scss'
-import { ReactComponent as Logo } from 'assets/logo.svg'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
 import classNames from 'classnames'
 import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri'
 
@@ -20,19 +20,19 @@ export default function Navbar() {
               [styles.selected]: window.location.pathname === '/',
             })}
           >
-            Pagina Inicial
-          </a>
-          <div className={styles.busca}></div>
-        </div>
-        <div className={styles.icones}>
-          <a href="/carrinho">
-            {window.location.pathname === '/carrinho' ? (
-              <RiShoppingCartFill {...iconeProps} />
-            ) : (
-              <RiShoppingCart2Line {...iconeProps} />
-            )}
+            Página inicial
           </a>
         </div>
+      </div>
+      <div className={styles.busca}></div>
+      <div className={styles.icones}>
+        <a href="/carrinho">
+          {window.location.pathname === '/carrinho' ? (
+            <RiShoppingCartFill {...iconeProps} />
+          ) : (
+            <RiShoppingCart2Line {...iconeProps} />
+          )}
+        </a>
       </div>
     </nav>
   )
